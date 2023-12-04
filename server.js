a04c40b66d5170396e17f9f625775f8d36ff01c0
Sam <49932341+ratemypraxis@users.noreply.github.com>
2023-12-03 23:08:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,43 +2,44 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
+//module for sending digital signal through the pins
 const Gpio = require('onoff').Gpio;
-
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-// tell server what pin the led is on
+//tell server what pin the led is on
 const ledPin = 18;
 const led = new Gpio(ledPin, 'out');
 
-// push webpage 
+//serve webpage 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-// a user has connected
+//a user has connected
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // a user has rung
+  //a user has rung
   socket.on('ring', () => {
     console.log('someone rung');
     
-    // turn on the LED for a sec by sending a HIGH event
+    //turn on the LED by sending a HIGH event
     led.writeSync(1);
+    //stop sending HIGH after a secong
     setTimeout(() => {
       led.writeSync(0);
     }, 1000);
   });
 
-  // a user has disconnected
+  //a user has disconnected
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
 });
 
-// run server
+//run server
 const port = 8080;
 server.listen(port, () => {
   console.log(`Server is running on port 8080`);
